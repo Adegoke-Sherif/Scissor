2724a8e5897b1287a74092e25af7e0f803205b76
Adegoke Sherif <shiffyadegoke@yahoo.com>
2024-08-16 14:23:41 +0100

    Final_commit

--- src/app.js
+++ src/app.js
@@ -3,53 +3,49 @@ import dotenv from "dotenv";
 import { connect } from "../src/database/db.js";
 import urlRouter from "./routes/url.routes.js";
 import passport from "./database/passport.js";
-import qrRoutes from "./routes/qr.code.js"
-import cors from "cors"
+import qrRoutes from "./routes/qr.code.js";
+import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
 
-
 dotenv.config();
-
 
 // Create a rate limiter
 const apiLimiter = rateLimit({
 	windowMs: 30 * 60 * 1000, // 30 minutes
 	max: 50, // limit each IP to 50 requests per windowMs
-	message: "Too many requests from this IP, please try again later."
+	message: "Too many requests from this IP, please try again later.",
 });
 
-// Apply the rate limiter to all API routes
-app.use("/api/", apiLimiter);
-
-//express app
+// Express app
 const app = express();
 const PORT = process.env.PORT || 4400;
-  
-//Middlewares
-app.use(express.json())
-app.use(passport.initialize())
-app.use(cors())
+
+// Middlewares
+app.use(express.json());
+app.use(passport.initialize());
+app.use(cors());
 app.use(helmet());
-app.use("/api/", apiLimiter); // Apply rate limiter to all API routes
 
-
-//routes
-app.use("/api/url", urlRouter)
-app.use("/api/qr", qrRoutes)
+// Apply the rate limiter only to API routes
+app.use("/api/url", apiLimiter, urlRouter);
+app.use("/api/qr", apiLimiter, qrRoutes);
 
 app.get("/", (req, res) => {
-	res.send("Welcome to URL Shortener")
-})
-
+	res.send("Welcome to URL Shortener");
+});
 
 app.all("*", (req, res) => {
 	res.status(404).json({ message: "Page not found" });
 });
 
-//connecting to the server and MongoDB
-connect().then(() => {
-	app.listen(PORT, () => {
-		console.log(`Server is running on PORT ${PORT}`);
+// Connecting to the server and MongoDB
+connect()
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server is running on PORT ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.error("Failed to connect to the database", error);
 	});
-});
